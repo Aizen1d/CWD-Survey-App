3ff75da2f1343fa95e9d213d6f2df99f2dd561ca
Aizen1d <ddreataza@gmail.com>
2024-04-02 20:59:19 +0800

    Client side react setup

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="flex justify-between items-center mb-6">
+        <NavLink to="/">
+        </NavLink>
+
+      </nav>
+    </div>
+  );
+}
