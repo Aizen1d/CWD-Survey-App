37005e18d1dbea44e03aba75bea8961ca8f98a98
Aizen1d <ddreataza@gmail.com>
2024-04-03 15:13:20 +0800

    Initial user model setup

--- api/models/User.model.js
+++ api/models/User.model.js
@@ -6,13 +6,11 @@ const userSchema = new Schema({
     type: String,
     required: true,
     unique: true,
-    trim: true,
   },
   email: {
     type: String,
     required: true,
     unique: true,
-    trim: true,
   },  
   password: {
     type: String,
