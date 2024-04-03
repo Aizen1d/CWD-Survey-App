5e6ce7c99fa7998e6e06b2e47540be0b98ae5e22
Aizen1d <ddreataza@gmail.com>
2024-04-03 13:47:17 +0800

    Transfer connection logic inside, restructure for readability

--- api/server.js
+++ api/server.js
@@ -1,19 +1,34 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 import dotenv from 'dotenv';
-import auth from "./routes/auth.js";
-
 dotenv.config({ path: './config.env'});
 
-const PORT = process.env.PORT;
-const app = express();
+// Routes
+import auth from "./routes/auth.js";
+import users from "./routes/users.js";
+
+// Conneciing to MongoDB
+mongoose
+  .connect(process.env.ATLAS_URI)
+  .then(() => {
+    console.log('Successful connection in MongoDB');
+  })
+  .catch((err) => {
+    console.log('Error: ' + err);
+  });
+
 const apiPrefix = "/api/v1";
+const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(apiPrefix, auth);
+
+app.use(`${apiPrefix}/auth`, auth);
+app.use(`${apiPrefix}/users`, users);
 
 // start the Express server
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
