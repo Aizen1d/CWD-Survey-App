9a5a012ab13d80bd3bbde444f696e23a9db4a4da
Aizen1d <ddreataza@gmail.com>
2024-04-03 15:14:29 +0800

    express urlencoded added

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ dotenv.config({ path: './config.env'});
 import auth from "./routes/auth.js";
 import users from "./routes/users.js";
 
-// Conneciing to MongoDB
+// Connecting to MongoDB
 mongoose
   .connect(process.env.ATLAS_URI)
   .then(() => {
@@ -23,6 +23,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(`${apiPrefix}/auth`, auth);
 app.use(`${apiPrefix}/users`, users);
