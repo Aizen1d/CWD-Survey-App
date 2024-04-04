7dfed29d4fa140e314373b960bf07afdad4e2d58
Aizen1d <ddreataza@gmail.com>
2024-04-04 23:02:49 +0800

    Authentication middleware

---
+++ api/middlewares/auth.middleware.js
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken';
+import asyncHanlder from 'express-async-handler';
+import User from '../models/User.model.js';
+
+import dotenv from 'dotenv';
+dotenv.config({ path: '../.env' });
+
+const protect = asyncHanlder(async (req, res, next) => {
+  let token;
+
+  token = req.cookies.jwt;
+
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.userId).select('-password');
+
+      next();
+    } 
+    catch (error) {
+      console.error(error);
+
+      res.status(401);
+      throw new Error('Not authorized, invalid token');
+    }
+  } 
+  else {
+    res.status(401);
+    throw new Error('Not authorized, no token');
+  }
+});
+
+export { protect };
