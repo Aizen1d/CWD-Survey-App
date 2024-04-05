f781075f7098b90504c36b672acf0b2fdc8954f6
Aizen1d <ddreataza@gmail.com>
2024-04-05 16:25:25 +0800

    Tanstack queryclient setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from  './components/Navbar'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import Login from './pages/Login'
+
+const queryClient = new QueryClient();
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <Navbar/> 
+    <QueryClientProvider client={queryClient}>
+      <Login/>
+    </QueryClientProvider>
   )
 }
 
