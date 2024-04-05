79b65dd6b96bb056eb1b12f9e44c607e23fad370
Aizen1d <ddreataza@gmail.com>
2024-04-05 16:21:19 +0800

    Protect middleware

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,13 @@
 import express from "express";
+
+// Controllers
 import { index } from "../controllers/user.controller.js";
+
+// Middleware
+import { protect } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/", index);
+router.route("/").get(index);  
 
 export default router;
