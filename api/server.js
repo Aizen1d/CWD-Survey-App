1053effb08f001218bd96d901607aa0f2ffce527
Aizen1d <ddreataza@gmail.com>
2024-04-05 16:20:16 +0800

    CORS for client url set

--- api/server.js
+++ api/server.js
@@ -25,7 +25,10 @@ mongoose
 const apiPrefix = "/api/v1";
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
