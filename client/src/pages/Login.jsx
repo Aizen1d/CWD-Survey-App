dd99acb4045076c97426c4938eb7a1a2da9458cd
Aizen1d <ddreataza@gmail.com>
2024-04-08 16:18:20 +0800

    UI updates, mutation when log in, fixes

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,58 @@
-import { TextField, Checkbox, Divider } from '@mui/material'
+import { useState } from 'react'
+import { useMutation } from '@tanstack/react-query'
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify'
+
+import { TextField, Checkbox } from '@mui/material'
+
+import { signinUser } from '../api/Auth.js'
+import FadeLoop from '../components/FadeLoop'
 
 const Login = () => { 
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const navigate = useNavigate()
+
+  const loginMutation = useMutation({
+      mutationFn: signinUser,
+      onSuccess: (data) => {
+        navigate('/dashboard')
+      },
+      onError: (error) => {
+        toast.error(error.response?.data?.message || 'An error occurred',{
+          toastId: error.response?.data?.message
+        })
+      }
+    })
+
+  const onSignInClick = () => {
+    loginMutation.mutate({ email, password })
+  } 
+
+  const items = [
+    { 
+      img: '/images/Login/Survey.png', 
+      maintext: 'Survey Anytime, Anywhere', 
+      subtext: 'Conduct surveys without limitations with CloudWalk Digital Survey app'
+    },
+    { 
+      img: '/images/Login/Experience.png', 
+      maintext: 'Experience On-The-Go Data Collection',
+      subtext: 'Embrace innovation with our user-friendly survey app' 
+    },
+    {
+      img: '/images/Login/Simplify.png', 
+      maintext: 'Simplify Your Data Journey',
+      subtext: 'Effortlessly gather and analyze data with CloudWalk'
+      },
+  ]; 
+
   return (
-    <div className="flex flex-col h-screen sm:flex-row">
-      <div className="flex-1 bg-white">
+    <div className="flex min-h-screen">
+      <div className="flex flex-1 pt-5 pb-5 bg-white justify-center items-center">
         
-        <div className="flex flex-col justify-center items-center mt-48 w-[450px] mx-auto">
-          <label className="font-[outfit] font-bold text-6xl text-black">
+        <div className="p-5 flex flex-col justify-center items-center md:p-0 md:w-[450px]">
+          <label className="font-[outfit] font-bold text-5xl text-black sm:text-6xl">
             Welcome Back
           </label>
 
@@ -18,6 +64,7 @@ const Login = () => {
             Email 
           </label>
           <TextField 
+            variant="outlined" 
             id='email-field' 
             style={{ width: '100%'}} 
             inputProps={{
@@ -26,13 +73,15 @@ const Login = () => {
               },
             }}
             label="Enter your email" 
-            variant="outlined" 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
 
           <label htmlFor="password-field" className='self-start mt-3 mb-2 font-[roboto] font-[400] text-[17px] text-[#3E3D3D]'>
             Password 
           </label>
           <TextField 
+            variant="outlined"
             id='password-field' 
             style={{ width: '100%'}} 
             inputProps={{
@@ -41,11 +90,12 @@ const Login = () => {
               },
             }}
             type="password" 
-            label="Enter your password" 
-            variant="outlined"
+            label="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
 
-          <div className='flex justify-between mt-2 w-[450px]'>
+          <div className='flex justify-between mt-2 w-[350px] sm:w-[450px]'>
             <div className='flex items-center'>
               <Checkbox 
                 style={{ paddingLeft: "2px"}} 
@@ -61,8 +111,9 @@ const Login = () => {
             </label>
           </div>
 
-          <button 
-            className="w-[445px] h-[50px] font-[roboto] font-[700] text-[17px] bg-[#0062FE] hover:bg-[#024dc7] text-white mt-2 rounded-[10px]">
+          <button disabled={loginMutation.isLoading} onClick={onSignInClick}
+            className="w-[345px] h-[40px] font-[roboto] font-[700] text-[17px] bg-[#0062FE] hover:bg-[#024dc7] text-white mt-2 rounded-[10px]
+                      sm:w-[445px] sm:h-[50px]">
             Log In
           </button>
 
@@ -75,28 +126,31 @@ const Login = () => {
             </label>
           </div>
 
-          <div class="flex items-center w-full mt-4 mb-3">
-            <div class="flex-1 border-t-2 border-gray-300"></div>
-            <span class="px-3 font-[roboto] font-[400] text-[17px] text-[#616161] bg-white">or continue</span>
-            <div class="flex-1 border-t-2 border-gray-300"></div>
+          <div className="flex items-center w-full mt-4 mb-3">
+            <div className="flex-1 border-t-2 border-gray-300"></div>
+            <span className="px-3 font-[roboto] font-[400] text-[17px] text-[#616161] bg-white">or continue</span>
+            <div className="flex-1 border-t-2 border-gray-300"></div>
           </div>
 
           <button 
-            className="flex justify-center items-center w-[445px] h-[50px] font-[roboto] font-[700] text-[17px] bg-white hover:bg-[#f2f2f2] text-black mt-3 rounded-[10px] border border-gray-400">
+            className="flex justify-center items-center w-[345px] h-[40px] font-[roboto] font-[700] text-[17px] bg-white hover:bg-[#f2f2f2] text-black mt-3 rounded-[10px] border border-gray-400
+                        sm:w-[445px] sm:h-[50px]">
             <img className='mr-3' src="/icons/Login/Google.png" alt="" />
             <span className='mt-1 font-[roboto] font-[700] text-[17px] text-[#616161]'>
               Log in with Google
             </span>
           </button>
-
         </div>
       </div>
 
-      <div className="flex-1 bg-[#0062FE]">
-
+      <div className="hidden xl:flex flex-1 justify-center bg-[#0062FE] rounded-tl-[40px] rounded-bl-[40px]">
+        <div className="flex flex-col justify-center items-center">
+          <FadeLoop items={items}>
+          </FadeLoop>
+        </div>
       </div>
+
     </div>
-    
   )
 } 
 
