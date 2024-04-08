048c6f47ea741ccac8657c6d7fc10d9558033984
Aizen1d <ddreataza@gmail.com>
2024-04-08 16:13:37 +0800

    Change samesite to none since auth is api

--- api/utils/generate.token.js
+++ api/utils/generate.token.js
@@ -8,7 +8,7 @@ const generateToken = (res, userId) => {
   res.cookie('jwt', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
-    sameSite: 'strict', // Prevent CSRF attacks
+    sameSite: 'none', // to work with cross-site requests with front-end
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
