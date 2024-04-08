b1716034afeb42e2c03843e849f1d28847856258
Aizen1d <ddreataza@gmail.com>
2024-04-08 16:16:16 +0800

    Toastify initialize

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,8 @@
+import React from 'react';
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import Login from './pages/Login'
 
@@ -7,6 +11,7 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <ToastContainer />
       <Login/>
     </QueryClientProvider>
   )
