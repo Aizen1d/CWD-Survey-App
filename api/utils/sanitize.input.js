031c770bfd21926f130ef9f66e32c1562fb2ae60
Aizen1d <ddreataza@gmail.com>
2024-04-09 15:14:17 +0800

    Sanitize util for object and string

---
+++ api/utils/sanitize.input.js
@@ -0,0 +1,32 @@
+import validator from "validator";
+
+const sanitizeString = (value) => {
+  let sanitizedValue;
+
+  if (typeof value === "string") {
+    sanitizedValue = validator.escape(value);
+    sanitizedValue = validator.trim(sanitizedValue);
+
+    return sanitizedValue;
+  }
+  else {
+    return value;
+  }
+}
+
+const sanitizeObject = (value) => {
+  let sanitizedObject = {};
+
+  if (typeof value === "object") {
+    for (const key in value) {
+      sanitizedObject[key] = sanitizeString(value[key]);
+    }
+
+    return sanitizedObject;
+  }
+  else {
+    return value;
+  }
+}
+
+export { sanitizeString, sanitizeObject };
