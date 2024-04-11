b6297243c8cc6566a7219f5f4b360ecf43ba726c
Aizen1d <ddreataza@gmail.com>
2024-04-11 08:57:06 +0800

    Restructured, transfer routes here

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,54 @@
 import React from 'react';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import Login from './pages/Login'
+import PublicRoutes from "./routes/PublicRoutes";
+import PrivateRoutes from "./routes/PrivateRoutes";
 
-const queryClient = new QueryClient();
+import UserRoutes from "./routes/UserRoutes";
+import AuthRoutes from "./routes/AuthRoutes";
+
+import Error404 from "./pages/Error404";
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       <ToastContainer />
-      <Login/>
-    </QueryClientProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<PublicRoutes />}>
+            {AuthRoutes.map((route, index) => (
+              <Route 
+                key={index}
+                path={route.path}
+                element={route.element}
+               />
+              ))
+            }
+          </Route>
+
+          <Route element={<PrivateRoutes />}>
+            {UserRoutes.map((route, index) => (
+              <Route 
+                key={index}
+                path={route.path}
+                element={route.element}
+               />
+              ))
+            }
+          </Route>
+          
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
