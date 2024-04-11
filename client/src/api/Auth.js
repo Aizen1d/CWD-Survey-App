490b8dbc91c941669a5f9450b15119a045efc481
Aizen1d <ddreataza@gmail.com>
2024-04-11 23:32:49 +0800

    Verify token api call

--- client/src/api/Auth.js
+++ client/src/api/Auth.js
@@ -2,6 +2,23 @@ import axiosApi from "./Axios";
 
 const authPrefix = "/auth";
 
+const verifyToken = async () => {
+  try {
+    const response = await axiosApi.post(`${authPrefix}/verify`);
+    const data = response.data;
+
+    if (data.verified) {
+      return true;
+    } 
+    else {
+      return false;
+    }
+  } 
+  catch (error) {
+    return false;
+  }
+}
+
 const signinUser = async ({ email, password }) => {
   try {
     const response = await axiosApi.post(`${authPrefix}/signin`, {
@@ -16,4 +33,21 @@ const signinUser = async ({ email, password }) => {
   }
 }
 
-export { signinUser };
+const signupUser = async ({ email, password }) => {
+  try {
+    const response = await axiosApi.post(`${authPrefix}/signup`, {
+      email: email,
+      password: password,
+    });
+
+    return response;
+  } 
+  catch (error) {
+    throw error;
+  }
+}
+
+export { 
+  verifyToken,
+  signinUser 
+};
