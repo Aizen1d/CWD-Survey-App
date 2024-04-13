54532edf400a3812d485dae69919c9e084919593
Aizen1d <ddreataza@gmail.com>
2024-04-13 15:47:35 +0800

    email availability route

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,17 +1,26 @@
 import express from "express";
+
+// Controllers
 import { 
   tokenVerifier,
   signin,
   signup,
-  signout 
+  signout,
+  isEmailAvailable 
 } from "../controllers/auth.controller.js";
+
+// Middleware
+import { protect } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/verify", tokenVerifier);
+
 router.post("/signin", signin)
 router.post("/signup", signup);
 router.post("/signout", signout)
+
+router.route("/email/:email").get(isEmailAvailable);
 
 export default router;
 
