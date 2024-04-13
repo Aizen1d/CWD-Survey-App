fda6dd855729feeac209c1acadae646760962f8f
Aizen1d <ddreataza@gmail.com>
2024-04-13 10:22:51 +0800

    Terms of Service content added

---
+++ client/src/components/TOS.jsx
@@ -0,0 +1,79 @@
+const TOS = () => {
+  return (
+    <div className="flex flex-col mt-3 ml-7 mr-7 space-y-2 font-[roboto] text-[15px] h-[520px] overflow-y-auto
+                    md:ml-10 md:mr-10 md:h-[550px]">
+      <span className="font-bold">1. Acceptance of Terms</span>
+      <span>
+        By utilizing our services, you are consenting to abide by these Terms and Conditions. 
+        Failure to agree with any aspect of these terms means you cannot access or use our services.
+      </span>
+
+      <span className="font-bold">2. Description of Services</span>
+      <span>
+        Our services encompass [a concise overview of the services offered]. 
+        We retain the authority to alter, suspend, or cease any aspect of our services at any given moment, 
+        with or without prior notification.
+      </span>
+
+      <span className="font-bold">3. User Accounts</span>
+      <span>
+        To access specific features of our services, you might need to create an account. 
+        It is your responsibility to keep your account and 
+        password confidential and to limit access to it. By creating an account, 
+        you agree to be accountable for all activities conducted under your account.
+      </span>
+
+      <span className="font-bold">4. User Conduct</span>
+      <span>
+        You agree not to utilize our services for any illegal activities or in a manner that contravenes these 
+        Terms and Conditions. Additionally, you undertake not to: 
+      </span>
+      <span>
+        - Engage in harassing, abusive, or harmful behavior towards other users.
+      </span>
+      <span>
+        - Infringe upon the rights of third parties.
+      </span>
+      <span>
+        - Disrupt or interfere with the functioning of our services.
+      </span>
+      <span>
+        - Engage in commercial solicitation through our services without obtaining our prior written consent.
+      </span>
+
+      <span className="font-bold">5. Intellectual Property</span>
+      <span>
+        The content and materials offered on our services, covering text, graphics, logos, images, and software, 
+        are owned by Cloudwalk Digital Inc. or its licensors, and are safeguarded by copyright, trademark, and 
+        other intellectual property regulations.
+      </span>
+      
+      <span className="font-bold">6. Limitation of Liability</span>
+      <span>
+        To the maximum extent allowed by law, Cloudwalk Digital Inc. is not responsible for any direct, indirect, incidental, 
+        special, or consequential damages resulting from or related to the utilization of our services.
+      </span>
+
+      <span className="font-bold">7. Indemnification</span>
+      <span>
+        You consent to indemnify and protect Cloudwalk Digital Inc., its affiliates, officers, directors, employees, 
+        and agents from any claims, liabilities, damages, losses, or expenses that may arise from or be related to your 
+        utilization of our services.
+      </span>
+
+      <span className="font-bold">8. Governing Law</span>
+      <span>
+        These Terms and Conditions will be regulated by and interpreted in compliance with the laws of [jurisdiction], 
+        without consideration for its conflict of law rules.
+      </span>
+
+      <span className="font-bold">9. Changes to Terms and Conditions</span>
+      <span>
+        We retain the authority to revise or amend these Terms and Conditions at any time without prior notification.
+        Your ongoing use of our services following any alterations indicates your acceptance of the updated Terms and Conditions.
+      </span>
+    </div>
+  )
+}
+
+export default TOS
