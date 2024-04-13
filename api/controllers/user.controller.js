789907346621b5d7aca1ebb5b945bc73daae8777
Aizen1d <ddreataza@gmail.com>
2024-04-13 15:46:25 +0800

    getUserByEmail added

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -6,4 +6,21 @@ const index = asyncHandler(async (req, res) => {
   res.status(200).json({'users': users});
 });
 
+const getUserByEmail = asyncHandler(async (req, res) => {
+  const email = req.params.email;
+  const user = await User.findOne({
+    email: email,
+  });
+
+  if (user) {
+    res.status(200).json({
+      user: user,
+    });
+  } 
+  else {
+    res.status(404);
+    throw new Error("User not found.");
+  }
+});
+
 export { index };
