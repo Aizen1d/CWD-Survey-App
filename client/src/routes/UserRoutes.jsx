f75a1308e8964028b30bc06b945caa26554dd5c3
Aizen1d <ddreataza@gmail.com>
2024-04-14 20:32:36 +0800

    Setup route added

--- client/src/routes/UserRoutes.jsx
+++ client/src/routes/UserRoutes.jsx
@@ -1,7 +1,12 @@
+import Setup from '../pages/User/Setup';
 import Dashboard from '../pages/User/Dashboard';
 import Survey from '../pages/User/Survey';
 
 const UserRoutes = [
+  {
+    path: '/setup',
+    element: <Setup />,
+  },
   {
     path: '/dashboard',
     element: <Dashboard />,
