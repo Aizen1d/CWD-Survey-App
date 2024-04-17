d03031ab7a4d919dfdafe73dcdbdc444540009a8
Aizen1d <ddreataza@gmail.com>
2024-04-17 23:54:08 +0800

    Redirect to setup if needed, protection route logics added

--- client/src/routes/PrivateRoutes.jsx
+++ client/src/routes/PrivateRoutes.jsx
@@ -1,25 +1,44 @@
-import { Outlet, Navigate, useLocation } from 'react-router-dom';
+import { Outlet, Navigate, useLocation, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { verifyToken } from '../api/Auth';
+import { isCurrentUserSetupDone } from '../api/User';
 import useAuthStore from '../stores/AuthStore';
 
 const PrivateRoutes = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isSetupDone, setIsSetupDone] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const authStore = useAuthStore();
   const location = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const checkAuth = async () => {
       const isAuthenticated = await verifyToken();
       setIsAuthenticated(isAuthenticated);
-      setIsLoading(false);
 
+      if (isAuthenticated) {
+        const isSetupDone = await isCurrentUserSetupDone();
+        setIsSetupDone(isSetupDone);
+      }
+  
+      setIsLoading(false);
       isAuthenticated ? authStore.login() : authStore.logout();
     }
-
+  
     checkAuth();
-  }, [location]);
+  }, [location]);  
+
+  useEffect(() => {
+    if (!isLoading && isAuthenticated) {
+      if (!isSetupDone && location.pathname !== '/setup') {
+        return navigate('/setup');
+      } 
+      else if (isSetupDone && location.pathname === '/setup') {
+        return navigate('/dashboard');
+      }
+    }
+  }, [isLoading]);
 
   if (isLoading) {
     return null;
