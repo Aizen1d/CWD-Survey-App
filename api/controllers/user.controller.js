42693ba85da82559357d6a4da540239686e0e6c5
Aizen1d <ddreataza@gmail.com>
2024-04-17 10:50:56 +0800

    Update user method added

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,9 +1,76 @@
 import User from "../models/User.model.js";
 import asyncHandler from "express-async-handler";
+import bcrypt from "bcryptjs";
+import { 
+  sanitizeObject 
+} from "../utils/validator.input.js";
 
 const index = asyncHandler(async (req, res) => {
   const users = await User.find({});
   res.status(200).json({'users': users});
+});
+
+const update = asyncHandler(async (req, res) => {
+  const user = req.user
+  const { email, password, firstName, lastName, birthday, contactNumber, 
+          gender, address, signature } = sanitizeObject(req.body);
+
+  if (user) {
+    user.firstName = firstName || user.firstName
+    user.lastName = lastName || user.lastName
+    user.birthday = birthday || user.birthday
+    user.gender = gender || user.gender
+    user.address = address || user.address
+    user.signature = signature || user.signature
+
+    if (email) {
+      if (!await user.isValidEmail(email)) {
+        res.status(404);
+        throw Error("Please enter a valid email address.");
+      } 
+
+      if (await user.existingEmail(email)) {
+        res.status(400);
+        throw Error("Email is already in use.");
+      }
+
+      user.email = email;
+    }
+
+    if (password) {
+      if (!await user.isGoodPassword(password)) {
+        res.status(400);
+        throw Error("Password must be at least 6 characters long.");
+      }
+      
+      user.password = password;
+    }
+
+    if (contactNumber) {
+      if (await user.existingContactNumber(contactNumber)) {
+        res.status(400);
+        throw Error("Contact number is already in use.");
+      }
+
+      user.contactNumber = contactNumber;
+    }
+    
+    const updatedUser = await user.save()
+    
+    if (updatedUser) {
+      res.status(200).json({
+        message: "User updated successfully."
+      });
+    }
+    else {
+      res.status(400)
+      throw new Error('Failed to update user.')
+    }
+  } 
+  else {
+    res.status(404)
+    throw new Error('User not found.')
+  }
 });
 
 const getUserByEmail = asyncHandler(async (req, res) => {
@@ -23,4 +90,8 @@ const getUserByEmail = asyncHandler(async (req, res) => {
   }
 });
 
-export { index };
+export { 
+  index,
+  update,
+  getUserByEmail
+};
