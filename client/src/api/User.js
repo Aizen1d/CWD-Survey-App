0713ea685edb9a64f449eafc983feea3831d2f0a
Aizen1d <ddreataza@gmail.com>
2024-04-17 23:48:53 +0800

    Update and isCurrentUserSetupDone added

--- client/src/api/User.js
+++ client/src/api/User.js
@@ -2,11 +2,33 @@ import axiosApi from './Axios'
 
 const usersPrefix = '/users';
 
-const getUsers = async () => {
+const index = async () => {
   try {
     const response = await axiosApi.get(`${usersPrefix}`);
 
     return response;
+  } 
+  catch (error) {
+    throw error;
+  }
+}
+
+const update = async ({ form }) => {
+  try {
+    const response = await axiosApi.put(`${usersPrefix}`, form);
+
+    return response;
+  } 
+  catch (error) {
+    throw error;
+  }
+}
+
+const isCurrentUserSetupDone = async () => {
+  try {
+    const response = await axiosApi.get(`${usersPrefix}/setup`);
+    
+    return response.data.status;
   } 
   catch (error) {
     throw error;
@@ -25,6 +47,8 @@ const getUserByEmail = async (email) => {
 }
 
 export { 
-  getUsers,
+  index,
+  update,
+  isCurrentUserSetupDone,
   getUserByEmail
 };
