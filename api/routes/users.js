26c7f2ca27e128d5996b6ba87e80718ecc865392
Aizen1d <ddreataza@gmail.com>
2024-04-17 10:49:07 +0800

    Add: update user, user/:email

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,11 @@
 import express from "express";
 
 // Controllers
-import { index } from "../controllers/user.controller.js";
+import { 
+  index,
+  update,
+  getUserByEmail
+} from "../controllers/user.controller.js";
 
 // Middleware
 import { protect } from "../middlewares/auth.middleware.js";
@@ -9,6 +13,8 @@ import { protect } from "../middlewares/auth.middleware.js";
 const router = express.Router();
 
 router.route("/").get(protect, index);  
-router.route("/:email").get(protect, index);
+router.route("/").put(protect, update);
+
+router.route("/:email").get(protect, getUserByEmail);
 
 export default router;
